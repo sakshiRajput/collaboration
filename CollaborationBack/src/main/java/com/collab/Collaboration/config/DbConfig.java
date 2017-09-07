fe3dbb57c5ff77970db69f97f6a2f48c391ba7fc
sakshiRajput <30687259+sakshiRajput@users.noreply.github.com>
2017-09-07 23:46:13 +0530

    day 1
    2nd commit -backend

--- CollaborationBack/src/main/java/com/collab/Collaboration/config/DbConfig.java
+++ CollaborationBack/src/main/java/com/collab/Collaboration/config/DbConfig.java
@@ -1,15 +1,53 @@
 package com.collab.Collaboration.config;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.collab.Collaboration.model.Blog;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages="com.collab")
 public class DbConfig {
 	
-	
-	
+
+	public DataSource getOracleDataSource(){
+		DriverManagerDataSource driverManagerDataSource = 
+				new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		driverManagerDataSource.setUrl("jdbc:oracle:thin:@//localhost:1522/XE ");
+		driverManagerDataSource.setUsername("hr");
+		driverManagerDataSource.setPassword("hr");
+		return driverManagerDataSource;
+		
+	}
+	@Bean
+	public SessionFactory getSessionFactory()
+	{
+		LocalSessionFactoryBuilder localSessionFactoryBuilder=new LocalSessionFactoryBuilder(getOracleDataSource());
+		localSessionFactoryBuilder.addProperties(getHibernateProperties());
+		localSessionFactoryBuilder.addAnnotatedClass(Blog.class);
+		return localSessionFactoryBuilder.buildSessionFactory();
+	}
+
+	public Properties getHibernateProperties()
+	{
+		Properties properties=new Properties();
+		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		properties.setProperty("hibernate.show_sql", "true");
+		properties.setProperty("hibernate.hbm2ddl.auto","update");
+		return properties;
+	}
 
 }
